bdd7671bc3f4bc8c87150082c630caaefd68af2a
Banafsheh <161360963+banafsheh66@users.noreply.github.com>
2026-09-25 22:38:11 +0300

    W2_6 assignment

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Program starting.\n\n")
+
+Color = input("Insert a hex color: \n")
+
+red = Color[1:3]
+green = Color[3:5]
+blue = Color[5:7]
+
+print("Colors\n")
+print("- Red ", Color[1:3],"\n")
+print("- Green ", Color[3:5],"\n")
+print("- Blue ", Color[5:7],"\n")
+
+print("program ending.\n")
