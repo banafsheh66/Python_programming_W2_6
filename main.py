39c6e6ae1fff67240b4a4b2729de87183a3ba0c7
Banafsheh <161360963+banafsheh66@users.noreply.github.com>
2026-09-25 22:49:41 +0300

    Output texting fixed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Program starting.\n\n")
+print("Program starting.\n")
 
 Color = input("Insert a hex color: \n")
 
@@ -6,9 +6,9 @@ red = Color[1:3]
 green = Color[3:5]
 blue = Color[5:7]
 
-print("Colors\n")
-print("- Red ", Color[1:3],"\n")
-print("- Green ", Color[3:5],"\n")
-print("- Blue ", Color[5:7],"\n")
+print("Colors")
+print("- Red ", Color[1:3])
+print("- Green ", Color[3:])
+print("- Blue ", Color[5:7])
 
-print("program ending.\n")
+print("program ending.")
